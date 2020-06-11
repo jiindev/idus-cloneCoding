51d99cd3dd8b40a0a116d3270f5d8bbee457e99d
jiindev <jiindev@gmail.com>
2020-06-12 00:30:42 +0900

    image태그에 키값으로 new Date() 추가

--- src/components/productItem.js
+++ src/components/productItem.js
@@ -9,7 +9,7 @@ const ProductItem = memo(({ data, noReview, artistSection }) => (
         <div>
         <Link to="/product" state={{data}}>
         <Thumbnail artistSection={artistSection}>
-            <img src={`/thumbnail/${data && data.thumbnailUrl}`} alt={`${data && data.title} 썸네일 이미지`}/>
+            <img src={`/thumbnail/${data && data.thumbnailUrl}`} alt={`${data && data.title} 썸네일 이미지`} key={new Date()}/>
         </Thumbnail>
         <Text>
             <div className="title">
