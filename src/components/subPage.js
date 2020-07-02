279a01e89e036c5cb74c4cf60dffb81613e77e65
jiindev <jiindev@gmail.com>
2020-07-02 22:00:10 +0900

    서브페이지 dummyData key값 변경하여 무한스크롤 오류 개선 테스트

--- src/components/subPage.js
+++ src/components/subPage.js
@@ -1,46 +1,53 @@
-import React, {memo, useEffect, useCallback, useState} from "react";
+import React, { memo, useEffect, useCallback, useState } from "react"
 import PropTypes from "prop-types"
-import Header from "./header";
-import {ProductList} from './productSection';
-import ProductItem from './productItem';
-import {getDummyDataRandomly} from '../data/data';
-import styled from 'styled-components';
+import Header from "./header"
+import { ProductList } from "./productSection"
+import ProductItem from "./productItem"
+import { getDummyDataRandomly } from "../data/data"
+import styled from "styled-components"
 
 const SubPage = memo(({ title, description, bannerBackground }) => {
-  const [dummyData, setDummyData] = useState(getDummyDataRandomly(20));
+  const [dummyData, setDummyData] = useState(getDummyDataRandomly(20))
 
   const onScroll = useCallback(() => {
-    if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300){
-      setTimeout(()=>{
-        setDummyData([...dummyData].concat(getDummyDataRandomly(20)));
-      }, 500);
+    if (
+      window.scrollY + document.documentElement.clientHeight >
+      document.documentElement.scrollHeight - 300
+    ) {
+      setTimeout(() => {
+        setDummyData([...dummyData].concat(getDummyDataRandomly(20)))
+      }, 500)
     }
-  }, [dummyData]);
+  }, [dummyData])
 
-  useEffect(()=>{
-    window.addEventListener('scroll', onScroll);
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll)
     return () => {
-      window.removeEventListener('scroll', onScroll);
+      window.removeEventListener("scroll", onScroll)
     }
-  }, [onScroll]);
+  }, [onScroll])
 
-  return(
+  return (
     <Wrap>
-      <Header title={title} description={description} bannerBackground={bannerBackground}/>
+      <Header
+        title={title}
+        description={description}
+        bannerBackground={bannerBackground}
+      />
       <div className="center">
-          <ProductList>
-              {dummyData.map((v, i)=>{
-                  return <ProductItem data={v} key={i}/>
-              })}
-          </ProductList>
+        <ProductList>
+          {dummyData.map((v, i) => {
+            return <ProductItem data={v} key={`${i}.${v.id}`} />
+          })}
+        </ProductList>
       </div>
     </Wrap>
-  );
-});
+  )
+})
 
 const Wrap = styled.div`
   padding-bottom: 50px;
-`;
+`
 
 SubPage.propTypes = {
   title: PropTypes.string,
